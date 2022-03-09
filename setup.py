ec5ebddd4ab8d7320dd178192b12572fcd616dcd
Alex Castillo <alex@neurosity.co>
2022-03-09 09:35:57 -0800

    :rocket: preparing setup for release

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='neurosity',
+    py_modules=['neurosity'],
     packages=find_packages(),
     version='1.0.0',
+    url='https://github.com/neurosity/neurosity-python-sdk',
+    python_requires='>=3.8',
     description='Official Neurosity Python SDK',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Alex Castillo',
     license='MIT',
+    install_requires=['pyrebase'],
+    classifiers=[
+        'Neurosity SDK',
+        'EEG',
+        'MIT License'
+    ],
 )
