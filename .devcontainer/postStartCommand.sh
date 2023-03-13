50e4cb7a7caaedf6bcfbbcd424c13d39eb0011b2
AJ Keller <aj@neurosity.co>
2023-03-13 00:10:28 +0000

    ADD: installs to dev container

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -8,4 +8,13 @@
 git config --global --add safe.directory '/workspaces'
 
 # Update pip
+echo "Updating pip"
 pip install --upgrade pip
+
+echo "Installing requirements"
+pip install -r requirements.txt
+
+echo "Installing development requirements"
+pip install -r dev-requirements.txt
+
+echo "Done with post start commands"
